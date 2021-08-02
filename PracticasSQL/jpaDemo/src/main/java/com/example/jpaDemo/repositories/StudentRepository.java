3e276f2196deedbae8b3a9202d35a4b9579741c7
matpereyra <matias.pereyra@mercadolibre.com>
2021-08-02 11:26:29 -0300

    Agregando todas las nuevas practicas hechas

---
+++ PracticasSQL/jpaDemo/src/main/java/com/example/jpaDemo/repositories/StudentRepository.java
@@ -0,0 +1,10 @@
+package com.example.jpaDemo.repositories;
+
+import com.example.jpaDemo.models.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long> {
+
+}
